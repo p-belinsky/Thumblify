630be72a9cede3e3985203b348f7e25f21679fdd
Phil Belinsky <philbelinsky@yahoo.com>
2026-01-13 09:30:01 -0500

    project complete

---
+++ server/controllers/UserController.ts
@@ -0,0 +1,35 @@
+import {Request, Response} from 'express';
+import Thumbnail from "../models/Thumbnail.js";
+
+
+
+export const getUserThumbnails = async (req: Request, res: Response) => {
+    try {
+
+        const {userId} = req.session;
+
+        const thumbnail = await Thumbnail.find({userId}).sort({createdAt: -1});
+
+        res.json({thumbnail});
+
+    }catch (error: any) {
+        console.log(error);
+        res.status(500).json({message: error.message});
+    }
+}
+
+export const getThumbnailById = async (req: Request, res: Response) => {
+    try {
+
+        const {userId} = req.session;
+        const {id} = req.params;
+
+        const thumbnail = await Thumbnail.findOne({userId, _id: id});
+
+        res.json({thumbnail});
+
+    }catch (error: any) {
+        console.log(error);
+        res.status(500).json({message: error.message});
+    }
+}
