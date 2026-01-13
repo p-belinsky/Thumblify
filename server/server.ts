39cddb691e7d9c8e71d4a87827d1714991513ecc
Phil Belinsky <philbelinsky@yahoo.com>
2026-01-13 09:37:16 -0500

    url updated in cors

--- server/server.ts
+++ server/server.ts
@@ -21,7 +21,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://thumblify-jet.vercel.app'],
     credentials: true,
 }));
 
